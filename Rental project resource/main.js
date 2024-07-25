eab9e4d6b9d58a087f52c5778e4a7087a3ad7c4e
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-07-26 01:19:41 +0600

    rent section added

--- Rental project resource/main.js
+++ Rental project resource/main.js
@@ -13,4 +13,32 @@ menuBtn.addEventListener("click", (e)=>{
 navLinks.addEventListener("click", (e)=>{
     navLinks.classList.remove("open");
     menuIcon.setAttribute("class", "fa-solid fa-bars");
-})
+});
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header-img img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header-content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header-content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header-links", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
